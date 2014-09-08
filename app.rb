bec96b10491b632df659b14a99b96f956cf82742
Andy Wong <alacritythief@gmail.com>
2014-09-08 14:56:55 -0400

    Added search function.

--- app.rb
+++ app.rb
@@ -15,3 +15,10 @@ get '/contacts/:id' do
   @contact = Contact.all.find(params[:id])
   erb :show
 end
+
+post '/search' do
+  first = params['firstname']
+  last = params['lastname']
+  @results = Contact.all.where("first_name like ? and last_name like?", "%#{first}%", "%#{last}%")
+  erb :search
+end
