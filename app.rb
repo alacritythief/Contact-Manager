b46cd9b57e086b3e6ca7549089c4d175d7d68b59
Andy Wong <alacritythief@gmail.com>
2014-09-08 16:23:17 -0400

    Added simple pagination. Noncore features complete.

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require "sinatra/activerecord"
-require 'pry'
+require 'sinatra/activerecord'
 
 require_relative 'models/contact'
 
 get '/' do
-  @contacts = Contact.all
+  @page = (params[:page]).to_i
 
+
+  if @page == nil || @page < 1
+    @page = 1
+  end
+
+  @contacts = Contact.all.limit(3).offset(@page * 3 - 3)
   erb :index
 end
 
